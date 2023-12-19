b9c96865052bbe7dfa50fdb08edad3a21ef5d5ed
OlgaBelickawa <145890221+OlgaBelickawa@users.noreply.github.com>
2023-12-19 10:06:24 +0500

    Favorite

---
+++ script.js
@@ -0,0 +1,11 @@
+const myName = "Ольга";
+const mySurname = "Белицкая";
+let myFavoriteDrink = "Вода";
+let myFavoriteAnimal = "Кошка";
+let myFavoriteProgrammingLanguage = "JavaScript";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
